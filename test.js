51fdec23eb69f1903c25b3044a787b82d7cbbb52
EmmanuelBahena <mannybahena04@gmail.com>
2023-05-08 12:11:07 -0500

    add arbitrary test example for integer and strings

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
 var myVariable = 3;
 
 console.log(myVariable);
+
+
+
+var monkeysJumping = 5;
+
+console.log(monkeysJumping);
+
+
+
+var wordsExample = ["pink", "marshmallows"];
+console.log(wordsExample);
